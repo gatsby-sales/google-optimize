ee04cebf5ca06585815979b32f43d1fbaa47ebcf
Chris LaRocque <larocque.christopher@gmail.com>
2022-09-26 14:10:46 -0400

    Update gatsby-ssr.tsx

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -13,11 +13,11 @@ const HeadComponents = [
           gtag('config', G-0M4VLT4RRY, { page_path: location ? location.pathname + location.search + location.hash : undefined })
         `}
     </script>
-    {/* <script>{`(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+    <script>{`(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
 h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
 (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
 })(window,document.documentElement,'async-hide','dataLayer',4000,
-{'CONTAINER_ID':true});`}</script> */}
+{'CONTAINER_ID':true});`}</script>
     <script
       key="optimize-init"
       src="https://www.googleoptimize.com/optimize.js?id=OPT-PBML2V3"
